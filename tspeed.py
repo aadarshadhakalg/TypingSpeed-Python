d5e6f1710569e437e0d0cd1a3d14611b0c4d9416
AyushPaudel <ap40021319@student.ku.edu.np>
2021-05-29 14:06:11 +0545

    optimized checking algorithm

--- tspeed.py
+++ tspeed.py
@@ -17,25 +17,13 @@ def choice(char):
 
 def check(sample, text):
 	# Error Calculation
-
-	if len(sample) <= len(text):
-		counter = 0
+	if sample == text:
 		error = 0
-		while counter < len(sample):
-			if sample[counter] == text[counter]:
-				error = error + 0
-			else:
-				error = error + 1
-			counter = counter + 1
-	elif len(text) <= len(sample):
-		counter = 0
-		error = 0
-		while counter < len(text):
-			if sample[counter] == text[counter]:
-				error = error + 0
-			else:
-				error = error + 1
-				counter = counter + 1
+	else:
+		max_ = sample if len(sample) > len(text) else text
+		min_ = text if sample == max_ else sample
+		x = [x for x, y in zip(max_, min_) if x != y]
+		error = len(x) + len(max_) - len(min_)
 	print("Errors: ", error, "characters")
 
 def main():
@@ -58,18 +46,17 @@ def main():
 			#This block creates a temporary file to store typed text.
 
 			sample=("Namaste! Nepal is a beautiful country situated in South Asia. (Heaven is myth, Nepal is real)")
-			print(len(sample))
+			print(sample)
 			t1 = time.perf_counter()
 
 			text=input("Please type above text here: ")
 			t2=time.perf_counter()
+			print("-"*80, end="\n \n")
 
 
 			#This block records the end time in seconds and prints end time.
 			y = datetime.datetime.now()
 			check(sample, text)
-			print("-"*80)
-			print("RESULT")
 
 			print("Typing end time: %s:%s:%s" % (y.hour, y.minute, y.second))
 
